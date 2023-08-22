0aa13364c73f859def2880c67896e8693c9f72ef
zimnikoff <sashazimnikoff@gmail.com>
2023-08-22 13:51:20 +0300

    architecture, AppRouter, Navbar, AppLink

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import './styles/index.scss'
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
+            <button onClick={toggleTheme}>toggle</button>
+        </div>
+    );
+};
+
+export default App;

8f3992e3b4f285de26f8e88f96e216b5a849f9f7
zimnikoff <sashazimnikoff@gmail.com>
2023-08-23 13:33:56 +0300

    add ThemeSwitcher, custom Button

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,26 +10,43 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
     };
 
     const cssLoader = {
-            test: /\.s[ac]ss$/i,
-            use: [
-                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            auto: (resPath: string) => resPath.includes('.module.'),
-                            localIdentName: isDev
-                                ? '[path][name]__[local]--[hash:base64:5]'
-                                : '[hash:base64:8]',
-                        },
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => resPath.includes('.module.'),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
                     },
                 },
-                "sass-loader",
-            ],
-            exclude: /node_modules/,
-        };
+            },
+            "sass-loader",
+        ],
+        exclude: /node_modules/,
+    };
+
+    const svgrLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
 
     return [
+        fileLoader,
+        svgrLoader,
         typescriptLoader,
         cssLoader,
     ]

a9fa0e7047f8ea0e5e24037c03635587805e2bae
zimnikoff <sashazimnikoff@gmail.com>
2023-08-25 10:40:52 +0300

    Added react-refresh-webpack-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions):webpack.WebpackPluginInstance[] {
     return [
@@ -16,5 +17,7 @@ export function buildPlugins({paths, isDev}: BuildOptions):webpack.WebpackPlugin
         new webpack.DefinePlugin({
             __IS_DEV__: isDev,
         }),
+        isDev && new webpack.HotModuleReplacementPlugin(),
+        isDev && new ReactRefreshWebpackPlugin(),
     ]
 }

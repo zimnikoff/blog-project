c747d97dc78c952a2b833a09651640de7d703f28
zimnikoff <sashazimnikoff@gmail.com>
2023-09-19 07:48:58 +0300

    update Navbar (add login button)

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,37 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            /
+export const Navbar = ({ className }: NavbarProps) => {
+    const [t] = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                {/* eslint-disable-next-line max-len */}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam commodi distinctio eaque, esse facere itaque laboriosam odio omnis pariatur placeat qui quos, rerum similique sint vitae voluptatem. Dolores, repudiandae!
+            </Modal>
         </div>
-    </div>
-);
+    );
+};

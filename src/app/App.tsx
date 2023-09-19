0e1333de851bd2790816bcd746c935eaf0e20973
zimnikoff <sashazimnikoff@gmail.com>
2023-09-19 05:39:18 +0300

    transfer style import from App.tsx to index.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';

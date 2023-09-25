b45c42031ec74e6f2f05daf4f08f68f20716dcda
zimnikoff <sashazimnikoff@gmail.com>
2023-09-25 09:22:02 +0300

    fix storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,17 +13,13 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    // config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    //     if (/svg/.test(rule.test as string)) {
-    //         return { ...rule, exclude: /\.svg$/i };
-    //     }
-    //
-    //     return rule;
-    // });
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
 
-    // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.filter((rule: RuleSetRule) => !(/svg/.test(rule.test as string)));
+        return rule;
+    });
 
     config.module.rules.push({
         test: /\.svg$/,

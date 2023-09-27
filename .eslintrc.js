da6e92a13e5137d4362817fbe64ba50fa0b0026e
zimnikoff <sashazimnikoff@gmail.com>
2023-09-27 14:08:46 +0300

    update eslint rules (react/self-closing-comp: off)

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,6 +56,7 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
         'no-param-reassign': 'off',
+        'react/self-closing-comp': 'off',
     },
     globals: {
         __IS_DEV__: 'readonly',

70f678ca61c8b27f1be3d75cb66f25baf17019c9
zimnikoff <sashazimnikoff@gmail.com>
2023-09-27 14:19:55 +0300

    update UI Modal (add laziness)

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -11,6 +11,7 @@ interface ModalProps {
     className?: string;
     isOpen?: boolean;
     onClose?: () => void;
+    lazy?: boolean;
 }
 
 const ANIMATION_DELAY = 200;
@@ -21,11 +22,19 @@ export const Modal = (props: ModalProps) => {
         className,
         isOpen,
         onClose,
+        lazy,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
     const { theme } = useTheme();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -62,6 +71,10 @@ export const Modal = (props: ModalProps) => {
         [cls.opened]: isOpen,
         [cls.closing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>

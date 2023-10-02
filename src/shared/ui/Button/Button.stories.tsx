3362009b3c6759478cf97bd5ce5dc72229128871
zimnikoff <sashazimnikoff@gmail.com>
2023-10-02 16:07:01 +0300

    update UI Button

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -93,3 +93,10 @@ SquareSizeXL.args = {
     square: true,
     size: ButtonSize.XL,
 };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    children: 'Text',
+    theme: ButtonTheme.OUTLINE,
+    disabled: true,
+};

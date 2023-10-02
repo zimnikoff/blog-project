edd3606be54203082303df6ae7d65cda803600fb
zimnikoff <sashazimnikoff@gmail.com>
2023-10-02 16:12:19 +0300

    update i18n config

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -3,12 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// TODO нужно правильно настроить i18n, сейчас появляется ошибка
-//  при первой загрузке, когда язык устанвавливается автоматически
-
 i18n
     .use(Backend)
-    // .use(LanguageDetector)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: localStorage.getItem('i18nextLng') ?? 'ru',

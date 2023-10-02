441551b5cf219171d4624deeeeebea8bcbe79d1c
zimnikoff <sashazimnikoff@gmail.com>
2023-10-02 16:11:56 +0300

    add UI Text

---
+++ src/shared/ui/Text/Text.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Text, TextTheme } from './Text';
+
+export default {
+    title: 'shared/Text',
+    component: Text,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Text>;
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    title: 'Title',
+    text: 'Text text text',
+};
+
+export const OnlyTitle = Template.bind({});
+OnlyTitle.args = {
+    title: 'Title',
+};
+
+export const OnlyText = Template.bind({});
+OnlyText.args = {
+    text: 'Text text text',
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    title: 'Title',
+    text: 'Text text text',
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTitleDark = Template.bind({});
+OnlyTitleDark.args = {
+    title: 'Title',
+};
+OnlyTitleDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTextDark = Template.bind({});
+OnlyTextDark.args = {
+    text: 'Text text text',
+};
+OnlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Error = Template.bind({});
+Error.args = {
+    title: 'Title',
+    text: 'Text text text',
+    theme: TextTheme.ERROR,
+};

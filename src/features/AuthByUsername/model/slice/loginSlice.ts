76546d135cd8abab68cb849a51a9031ba0d7a5fc
zimnikoff <sashazimnikoff@gmail.com>
2023-10-26 09:08:06 +0300

    update loginSlice (remove unused vars)

--- src/features/AuthByUsername/model/slice/loginSlice.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -21,11 +21,11 @@ export const loginSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
-            .addCase(loginByUsername.pending, (state, action) => {
+            .addCase(loginByUsername.pending, (state) => {
                 state.error = undefined;
                 state.isLoading = true;
             })
-            .addCase(loginByUsername.fulfilled, (state, action) => {
+            .addCase(loginByUsername.fulfilled, (state) => {
                 state.isLoading = false;
             })
             .addCase(loginByUsername.rejected, (state, action) => {
@@ -37,5 +37,3 @@ export const loginSlice = createSlice({
 
 export const { actions: loginActions } = loginSlice;
 export const { reducer: loginReducer } = loginSlice;
-
-export default loginSlice.reducer;

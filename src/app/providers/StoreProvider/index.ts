2f338a40b35e2484c37d6f249d22b03ae82a0899
zimnikoff <sashazimnikoff@gmail.com>
2023-10-31 10:32:56 +0300

    add useAppDispatch

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,10 +1,11 @@
-import type { StateSchema, ReduxStoreWithManager } from './config/StateSchema';
+import type { ReduxStoreWithManager, StateSchema } from './config/StateSchema';
 import { StoreProvider } from './ui/StoreProvider';
-import { createReduxStore } from './config/store';
+import { AppDispatch, createReduxStore } from './config/store';
 
 export {
     StoreProvider,
     createReduxStore,
     StateSchema,
     ReduxStoreWithManager,
+    AppDispatch,
 };

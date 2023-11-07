90339d6e87229bef8b97f21755ceff308ff0840d
zimnikoff <sashazimnikoff@gmail.com>
2023-11-07 14:43:30 +0300

    update test environment, fix tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
 import i18nForTests from '../../../config/i18n/i18nForTests';
 
 export interface componentRenderOptions {
@@ -15,12 +14,12 @@ export function componentRender(component: ReactNode, options: componentRenderOp
     const { route = '/', initialState } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
 }

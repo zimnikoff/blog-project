2cd3ca7ae53e04637beee67866bd200b9519058c
zimnikoff <sashazimnikoff@gmail.com>
2023-11-13 15:35:27 +0300

    fix avatar stories

--- src/shared/ui/Avatar/Avatar.stories.tsx
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { Avatar } from './Avatar';
-import AvatarImg from './storybook.jpg';
+import AvatarImg from '../../assets/tests/storybook.jpg';
 
 export default {
     title: 'shared/Avatar',

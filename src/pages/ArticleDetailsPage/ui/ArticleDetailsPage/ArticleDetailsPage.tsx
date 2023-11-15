5c850bb4dc712c15d53d8ea6f9d2a50d409d98c3
zimnikoff <sashazimnikoff@gmail.com>
2023-11-15 11:53:45 +0300

    add articles page, article details page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './ArticleDetailsPage.module.scss';
+
+interface ArticleDetailsPageProps {
+    className?: string;
+}
+
+const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
+    const { t } = useTranslation('article');
+
+    return (
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            ARTICLE DETAILS PAGE
+        </div>
+    );
+};
+
+export default memo(ArticleDetailsPage);
---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = ({ className }: ArticlesPageProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+            ARTICLES PAGE
+        </div>
+    );
+};
+
+export default memo(ArticlesPage);

9c2d0774b3f8c9b37d76fb476226b107edff9e68
zimnikoff <sashazimnikoff@gmail.com>
2023-11-17 17:15:58 +0300

    add UI Code

---
+++ src/shared/ui/Code/Code.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Code } from './Code';
+
+export default {
+    title: 'shared/Code',
+    component: Code,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Code>;
+
+const Template: ComponentStory<typeof Code> = (args) => <Code {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+    code: 'import React from \'react\';\n'
+        + 'import { ComponentStory, ComponentMeta } from \'@storybook/react\';\n'
+        + 'import { Code } from \'./Code\';\n'
+        + '\n'
+        + 'export default {\n'
+        + '    title: \'shared/Code\',\n'
+        + '    component: Code,\n'
+        + '    argTypes: {\n'
+        + '        backgroundColor: { control: \'color\' },\n'
+        + '    },\n'
+        + '} as ComponentMeta<typeof Code>;',
+};
+Normal.decorators = [];
+
+export const Dark = Template.bind({});
+Dark.args = {
+    code: 'import React from \'react\';\n'
+        + 'import { ComponentStory, ComponentMeta } from \'@storybook/react\';\n'
+        + 'import { Code } from \'./Code\';\n'
+        + '\n'
+        + 'export default {\n'
+        + '    title: \'shared/Code\',\n'
+        + '    component: Code,\n'
+        + '    argTypes: {\n'
+        + '        backgroundColor: { control: \'color\' },\n'
+        + '    },\n'
+        + '} as ComponentMeta<typeof Code>;',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Afternoon = Template.bind({});
+Afternoon.args = {
+    code: 'import React from \'react\';\n'
+        + 'import { ComponentStory, ComponentMeta } from \'@storybook/react\';\n'
+        + 'import { Code } from \'./Code\';\n'
+        + '\n'
+        + 'export default {\n'
+        + '    title: \'shared/Code\',\n'
+        + '    component: Code,\n'
+        + '    argTypes: {\n'
+        + '        backgroundColor: { control: \'color\' },\n'
+        + '    },\n'
+        + '} as ComponentMeta<typeof Code>;',
+};
+Afternoon.decorators = [ThemeDecorator(Theme.AFTERNOON)];

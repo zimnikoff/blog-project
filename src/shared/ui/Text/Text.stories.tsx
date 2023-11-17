951285fd8c0980e1f893aefc9c0db2dbe883897d
zimnikoff <sashazimnikoff@gmail.com>
2023-11-17 17:08:57 +0300

    update UI Text (add sizes)

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Text, TextTheme } from './Text';
+import { Text, TextSize, TextTheme } from './Text';
 
 export default {
     title: 'shared/Text',
@@ -54,4 +54,11 @@ Error.args = {
     title: 'Title',
     text: 'Text text text',
     theme: TextTheme.ERROR,
+};
+
+export const SizeL = Template.bind({});
+SizeL.args = {
+    size: TextSize.L,
+    title: 'Title',
+    text: 'Text text text',
 };

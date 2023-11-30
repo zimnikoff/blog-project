0bba7ff9e4b73f06be9310c47a99806588ab21e4
zimnikoff <sashazimnikoff@gmail.com>
2023-11-30 19:26:22 +0300

    fix infinite scroll

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -10,9 +10,12 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }:UseInfini
     useEffect(() => {
         let observer: IntersectionObserver | null = null;
 
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
+
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -23,13 +26,12 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }:UseInfini
                 }
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
         }
 
         return () => {
-            if (observer) {
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(triggerRef.current);
+            if (observer && triggerElement) {
+                observer.unobserve(triggerElement);
             }
         };
     }, [callback, triggerRef, wrapperRef]);

2980771acb071b94f20f92c614404182b18cfe06
zimnikoff <sashazimnikoff@gmail.com>
2023-12-01 10:51:46 +0300

    i18n logs off

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -9,7 +9,7 @@ i18n
     .use(initReactI18next)
     .init({
         fallbackLng: localStorage.getItem('i18nextLng') ?? 'ru',
-        debug: __IS_DEV__,
+        // debug: __IS_DEV__,
         interpolation: {
             escapeValue: false,
         },

c371cf0a14435bdb0cffe11e7143594997422817
zimnikoff <sashazimnikoff@gmail.com>
2024-01-18 11:52:44 +0300

    update Navbar (add menu)

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -8,6 +8,8 @@ import { LoginModal } from 'features/AuthByUsername';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Dropdown } from 'shared/ui/Dropdown/Dropdown';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -36,14 +38,24 @@ export const Navbar = memo(({ className }: NavbarProps) => {
         return (
             <header className={classNames(cls.Navbar, {}, [className])}>
                 <Text className={cls.appName} title={t('SmartBlogger')} theme={TextTheme.SECONDARY} />
-                <AppLink to={RoutePath.article_create} theme={AppLinkTheme.THIRD}>{t('Создать статью')}</AppLink>
-                <Button
-                    theme={ButtonTheme.CLEAR_INVERTED}
-                    className={cls.links}
-                    onClick={onLogout}
-                >
-                    {t('Выйти')}
-                </Button>
+                <AppLink to={RoutePath.article_create} theme={AppLinkTheme.THIRD}>
+                    {t('Создать статью')}
+                </AppLink>
+                <Dropdown
+                    items={[
+                        {
+                            content: t('Выйти'),
+                            onClick: onLogout,
+                        },
+                        {
+                            content: t('Профиль'),
+                            href: RoutePath.profile + authData.id,
+                        },
+                    ]}
+                    trigger={<Avatar size={40} src={authData.avatar} />}
+                    className={cls.dropdown}
+                    direction="bottom-left"
+                />
             </header>
         );
     }

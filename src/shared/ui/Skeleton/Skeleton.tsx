97be9a7587a2f6cf29e06e45bad3615d6ae37a8d
zimnikoff <sashazimnikoff@gmail.com>
2024-01-25 09:55:09 +0300

    update UI Skeleton (add border-radius prop)

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -7,6 +7,7 @@ interface SkeletonProps {
     width?: number | string;
     height?: number | string;
     isRound?: boolean;
+    borderRadius?: number;
 }
 
 export const Skeleton = memo((props: SkeletonProps) => {
@@ -15,12 +16,13 @@ export const Skeleton = memo((props: SkeletonProps) => {
         width,
         height,
         isRound,
+        borderRadius = 0,
     } = props;
 
     const styles: CSSProperties = {
         width,
         height,
-        borderRadius: isRound ? '50%' : undefined,
+        borderRadius: (borderRadius > 0 ? `${borderRadius}px` : undefined) || (isRound ? '50%' : undefined),
     };
 
     return (

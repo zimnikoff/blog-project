26345094b0dd411944b11aad720772cce0cbcf61
zimnikoff <sashazimnikoff@gmail.com>
2024-01-25 11:27:06 +0300

    add UI Drawer

---
+++ src/shared/ui/Drawer/Drawerr.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Drawer } from './Drawer';
+
+export default {
+    title: 'shared/Drawer',
+    component: Drawer,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Drawer>;
+
+const Template: ComponentStory<typeof Drawer> = (args) => <Drawer {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [];

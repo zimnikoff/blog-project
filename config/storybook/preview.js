fe8deecc50333f6173bdf85381019447dbc27cab
zimnikoff <sashazimnikoff@gmail.com>
2024-01-31 09:00:51 +0300

    update packages, fix problems

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,9 @@
 import { addDecorator } from '@storybook/react';
-import { StyleDecorator } from '@/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { RouterDecorator } from '@/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { SuspenseDecorator } from '@/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { Theme } from '@/shared/const/theme';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -17,9 +17,9 @@ export const parameters = {
     themes: {
         default: 'light',
         list: [
-            { name: 'light', class: Theme.LIGHT, color: '#fff' },
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
             { name: 'dark', class: Theme.DARK, color: '#000000' },
-            { name: 'afternoon', class: Theme.AFTERNOON, color: '#9a9a9a' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
         ],
     },
 };
--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
-import withMock from 'storybook-addon-mock';
 import { NotificationList } from './NotificationList';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
@@ -11,7 +9,6 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    decorators: [withMock],
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;

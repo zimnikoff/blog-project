a30b25f771a06f828af2587fb3a022912ba872d2
zimnikoff <sashazimnikoff@gmail.com>
2024-02-02 07:56:56 +0300

    add feature flag concept

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -13,6 +13,8 @@ import { ArticleDetailsComments } from '@/pages/ArticleDetailsPage/ui/ArticleDet
 import cls from './ArticleDetailsPage.module.scss';
 import { articleDetailsPageReducer } from '../../model/slices';
 import { ArticleRating } from '@/features/articleRating';
+import { getFeatureFlag } from '@/shared/lib/features';
+import { Counter } from '@/entities/Counter';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -24,6 +26,8 @@ const reducersList: ReducersList = {
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
     const { id } = useParams<{ id: string }>();
+    const isArticleRatingEnabled = getFeatureFlag(('isArticleRatingEnabled'));
+    const isCounterEnabled = getFeatureFlag(('isCounterEnabled'));
 
     if (!id) {
         return null;
@@ -35,7 +39,8 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
                 <VStack gap="16" max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    <ArticleRating articleId={id} />
+                    {isCounterEnabled && <Counter />}
+                    {isArticleRatingEnabled && <ArticleRating articleId={id} />}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
